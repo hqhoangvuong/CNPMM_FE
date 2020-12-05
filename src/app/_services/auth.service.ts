dc0a2cb4b420846929529617c27d0ab2af06a0a9
Vuong Huynh <hoangvuong19991964@gmail.com>
2020-12-05 13:50:46 +0700

    change end-point

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'http://103.153.73.107:8080/api/Auth/';
+const AUTH_API = 'http://hieuvm.xyz:8080/api/Auth/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
--- src/app/_services/directory.service.ts
+++ src/app/_services/directory.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'http://103.153.73.107:8080/api/Directory';
+const API_URL = 'http://hieuvm.xyz:8080/api/Directory';
 @Injectable({
   providedIn: 'root'
 })
